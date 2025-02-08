f6b07bcd1b274a9d751c58ae72f6b7228ca12f17
AbdulRahman Okasha <a.majdy41@gmail.com>
2025-02-08 17:11:57 +0200

    update the file

--- MySQLServer.py
+++ MySQLServer.py
@@ -1,14 +1,16 @@
 import mysql.connector
 
-db = mysql.connector.connect(
-    host='localhost',
-    user='root',
-    password='Mmn#2326681',
-)
-
-mycursor = db.cursor()
-
-mycursor.execute('CREATE DATABASE IF NOT EXISTS alx_book_store')
-
-mycursor.close()
-db.close()
+try:
+    db = mysql.connector.connect(
+        host='localhost',
+        user='root',
+        password='Mmn#2326681',
+    )
+except mysql.connector.Error:
+    print("Couldn't connect to the database")
+else:
+    mycursor = db.cursor()
+    mycursor.execute('CREATE DATABASE IF NOT EXISTS alx_book_store')
+finally:
+    mycursor.close()
+    db.close()
